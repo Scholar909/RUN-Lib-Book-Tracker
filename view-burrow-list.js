538dd58979e9be9e3f8e6d5c148fe17ff00406e5
Scholar909 <enehogbole909@gmail.com>
2025-10-30 10:10:01 +0100

    Update view-burrow-list.js

--- view-burrow-list.js
+++ view-burrow-list.js
@@ -1,9 +1,3 @@
-if (!/Mobi|Android/i.test(navigator.userAgent)) {
-  document.body.innerHTML = "<h2>This app is only available on mobile devices.</h2>";
-} else {
-  // Your other JS code and imports go below this line
-}
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getFirestore, collection, getDocs, query, where, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
